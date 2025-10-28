6535a0772b5c426b5f46bab5b192e29155493c7c
karthickramalagare <karthickramalagr@gmail.com>
2025-10-28 21:28:15 +0530

    chor:Minor Change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base:"/Dr.S.Rengaraj-Portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
