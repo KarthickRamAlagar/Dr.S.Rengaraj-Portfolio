1914f6a11089ed2be07d8a01bcbbcf7c2dc6bb16
karthickramalagare <karthickramalagr@gmail.com>
2025-10-29 23:32:00 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { shadesOfPurple } from "@clerk/themes";
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
-
 function App() {
   return (
     <ClerkProvider
