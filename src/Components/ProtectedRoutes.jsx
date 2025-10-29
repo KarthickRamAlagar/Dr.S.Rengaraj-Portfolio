a3de169ddd8de2abd372b6f460143647e7fe7337
karthickramalagare <karthickramalagr@gmail.com>
2025-10-29 12:27:56 +0530

    Chor

--- src/Components/ProtectedRoutes.jsx
+++ src/Components/ProtectedRoutes.jsx
@@ -1,23 +1,3 @@
-// import { useUser } from "@clerk/clerk-react";
-// import { Navigate, useLocation } from "react-router-dom";
-
-// const ProtectedRoutes = ({ children }) => {
-//   const { isLoaded, isSignedIn, user } = useUser();
-//   const { pathname } = useLocation();
-
-//   if (!isLoaded) return null;
-//   if (!isSignedIn) return <Navigate to="/Dr.Rengaraj/?sign-in=true" replace />;
-
-//   // Optional: role-based redirect
-//   if (user && !user.unsafeMetadata?.role && pathname !== "/Dr.Rengaraj/") {
-//     return <Navigate to="/Dr.Rengaraj/" />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoutes;
-
 import { useUser } from "@clerk/clerk-react";
 import { Navigate, useLocation } from "react-router-dom";
 
