50c6f05bcbc76c13d9d995796104812047d710ce
karthickramalagare <karthickramalagr@gmail.com>
2025-10-29 11:15:43 +0530

    Fix: Routing Bug

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,62 +1,4 @@
-// // src/router.jsx
-// import { createBrowserRouter } from "react-router-dom";
-// import AppLayout from "../layouts/AppLayout";
-// import Home from "../Pages/Home";
-// import Bio from "../Pages/Bio";
-// import ProtectedRoutes from "../Components/ProtectedRoutes";
-// import NotFound from "../Pages/NotFound";
-// import EducationSection from "@/Components/Education/EducationSection";
-// import ExperienceSection from "@/Components/Experience/ExperienceSection";
-// import Contact from "@/Components/Contact/Contact";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/Dr.Rengaraj/",
-//     element: <AppLayout />,
-//     children: [
-//       {
-//         index: true,
-//         element: <Home />,
-//       },
-//       {
-//         path: "Bio",
-//         element: <Bio />,
-//       },
-//       {
-//         path: "Education",
-//         element: (
-//           <ProtectedRoutes>
-//             <EducationSection />
-//           </ProtectedRoutes>
-//         ),
-//       },
-//       {
-//         path: "Experience",
-//         element: (
-//           <ProtectedRoutes>
-//             <ExperienceSection />
-//           </ProtectedRoutes>
-//         ),
-//       },
-//       {
-//         path: "Contact",
-//         element: (
-//           <ProtectedRoutes>
-//             <Contact />
-//           </ProtectedRoutes>
-//         ),
-//       },
-//     ],
-//   },
-//   {
-//     path: "*",
-//     element: <NotFound />,
-//   },
-// ]);
-
-// export default router;
-
-// src/routes/router.jsx
+// src/router.jsx
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "../layouts/AppLayout";
 import Home from "../Pages/Home";
@@ -66,11 +8,10 @@ import NotFound from "../Pages/NotFound";
 import EducationSection from "@/Components/Education/EducationSection";
 import ExperienceSection from "@/Components/Experience/ExperienceSection";
 import Contact from "@/Components/Contact/Contact";
-import { SignIn, SignUp } from "@clerk/clerk-react";
 
 const router = createBrowserRouter([
   {
-    path: "/Dr.Rengaraj/",
+    path: "/",
     element: <AppLayout />,
     children: [
       {
@@ -104,15 +45,6 @@ const router = createBrowserRouter([
             <Contact />
           </ProtectedRoutes>
         ),
-      },
-      // 👇 Clerk Auth Routes
-      {
-        path: "sign-in",
-        element: <SignIn routing="path" path="/Dr.Rengaraj/sign-in" />,
-      },
-      {
-        path: "sign-up",
-        element: <SignUp routing="path" path="/Dr.Rengaraj/sign-up" />,
       },
     ],
   },
