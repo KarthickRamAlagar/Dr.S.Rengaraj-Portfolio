20a91c6ba5bd47a1f75bfeab41cc72ab286f9533
karthickramalagare <karthickramalagr@gmail.com>
2025-10-29 23:17:35 +0530

    Fix:Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,26 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { shadesOfPurple } from "@clerk/themes";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env file");
+}
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      appearance={{ baseTheme: shadesOfPurple }}
+      signInUrl="/Dr.S.Rengaraj-Portfolio/?sign-in=true"
+      signUpUrl={null}
+      afterSignInUrl="/Dr.S.Rengaraj-Portfolio/"
+      afterSignOutUrl="/Dr.S.Rengaraj-Portfolio/"
+    >
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  );
 }
 
 export default App;
-
-// import { RouterProvider } from "react-router-dom";
-// import router from "./routes/router";
-// import { ClerkProvider } from "@clerk/clerk-react";
-
-// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-// function App() {
-//   return (
-//     <ClerkProvider
-//       publishableKey={PUBLISHABLE_KEY}
-//       signInUrl="/Dr.S.Rengaraj-Portfolio/?sign-in=true"
-//       signUpUrl={null} // disables sign-up
-//       afterSignInUrl="/Dr.S.Rengaraj-Portfolio/"
-//       afterSignOutUrl="/Dr.S.Rengaraj-Portfolio/"
-//     >
-//       <RouterProvider router={router} />
-//     </ClerkProvider>
-//   );
-// }
-
-// export default App;
