8446fe5b529b038ec650f11bfed40214ce8444cf
karthickramalagare <karthickramalagr@gmail.com>
2025-10-29 22:38:18 +0530

    Fix:Navigation Issues

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -5,7 +5,7 @@ import SignInRedirectHandler from "../Components/SignInRedirectHandler";
 const AppLayout = () => {
   const location = useLocation();
 
-  const hideNavbar = location.pathname === "/Dr.Rengaraj/BioGraphy";
+  const hideNavbar = location.pathname === "/Dr.S.Rengaraj-Portfolio/BioGraphy";
 
   return (
     <div className="bg-radial-dark text-white min-h-screen w-screen overflow-x-hidden flex flex-col">
